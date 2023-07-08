812412d06291e5bf0e133475fe7ca76e1b517d47
Deemmmy <121949485+Deemmmy@users.noreply.github.com>
2023-07-08 13:32:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+    keyboard: true,
+  });
+
+  const questions = document.getElementsByClassName("question");
+
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", function() {
+    const answer = this.querySelector(".answer");
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+  });
+}
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".burger-menu_button", ".burger-menu_lines");
+  let links = menu.find(".burger-menu_link");
+  let overlay = menu.find(".burger-menu_overlay");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("burger-menu_active");
+
+    if (menu.hasClass("burger-menu_active")) {
+      $("body").css("overlow", "hidden");
+    } else {
+      $("body").css("overlow", "visible");
+    }
+  }
+}
+
+burgerMenu(".burger-menu");
